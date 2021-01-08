99f6f08550acd1cda30034d78a720f06b7b23ad3
0869996807 <75104376+0869996807@users.noreply.github.com>
2021-01-08 10:40:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
 	<title>Xin chào</title>
 	<meta charset="utf-8">
 </head>
-<a href="https://nhaaptech1.herokuapp.com/bai1.html">Bài1</a>
+<a href="https://nhaaptech1.herokuapp.com/bai1.html">Bài 1</a>
+<a href="https://nhaaptech1.herokuapp.com/bai2.1.html">Bài 2.1</a>
+<a href="https://nhaaptech1.herokuapp.com/GalleryOne.html">Bài 2.2</a>
+<a href="https://nhaaptech1.herokuapp.com/GalleryTwo.html">Bài 2.3</a>
 <body>
 
 </body>
