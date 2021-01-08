b86db2d8a51d44bd67edb658c61503572a4a9d85
0869996807 <75104376+0869996807@users.noreply.github.com>
2021-01-08 10:36:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-# html
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Xin chào</title>
+	<meta charset="utf-8">
+</head>
+<a href="https://nhaaptech1.herokuapp.com/bai1.html">Bài1</a>
+<body>
+
+</body>
+</html>
