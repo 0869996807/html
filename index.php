5b4e1e018f9a5a6341a5cd96ae62f0d7a0888635
0869996807 <75104376+0869996807@users.noreply.github.com>
2021-01-11 11:11:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <a href="https://nhaaptech1.herokuapp.com/GalleryOne.html" target="_blank"><h2>Bài 2.2</h2></a>
 <a href="https://nhaaptech1.herokuapp.com/GalleryTwo.html" target="_blank"><h2>Bài 2.3</h2></a>
 <a href="https://nhaaptech1.herokuapp.com/home.html" target="_blank"><h2>Bài 2.4</h2></a>
+<a href="touristAttractions.html" target="_blank"><h2>Bài 3.1</h2></a>
 
 
 </body>
