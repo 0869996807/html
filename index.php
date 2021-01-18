4884eaafc1663e4c2d48d02cb236b37e435b6c0b
0869996807 <75104376+0869996807@users.noreply.github.com>
2021-01-18 11:51:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 <a href="https://nhaaptech1.herokuapp.com/touristAttractions.html" target="_blank"><h2>Bài 3.1</h2></a>
 <a href="https://nhaaptech1.herokuapp.com/bai3.1.html" target="_blank"><h2>Bài 4.1</h2></a>
 <a href="https://nhaaptech1.herokuapp.com/aboutus.html" target="_blank"><h2>Bài 5.1</h2></a>
+<a href="https://nhaaptech1.herokuapp.com/bai6.1.html" target="_blank" title="Giai phuong trinh bac 1, 2, tim max cua 5 so"><h2>Bài 6.1</h2></a>
 
 
 
